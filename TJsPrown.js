e513bad4f03d71d2671dd0a255ba4d4ffa792e15
tve <tve@karelia.ru>
2019-02-12 21:40:44 +0300

    Добавлены функции

--- TJsPrown.js
+++ TJsPrown.js
@@ -14,3 +14,80 @@ function MakeCatchyQuotes()
    return Result;
 }
 
+function getTime(secs) {
+	var sep = ':'; //separator character
+	var hours,minutes,seconds,time,am_pm;
+	var now = new Date();
+	hours = now.getHours();
+	if (hours < 12) {
+		am_pm = 'am';
+	} else {
+		am_pm = 'pm';
+	}
+	hours = hours % 12;
+	if (hours === 0) {
+		hours = 12;
+	} 
+	time = hours;
+	minutes = now.getMinutes();
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	time += sep + minutes;
+	if (secs) {
+		seconds = now.getSeconds();
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		time += sep + seconds;
+	} 
+	return time + ' ' + am_pm;
+}
+        
+  function TurnOnHotkeys(){
+  
+      document.onkeydown = function(e) {
+
+      // Ctrl+I  Показать установленные настройки сайта
+      if (e.ctrlKey && e.keyCode == 'I'.charCodeAt(0)) 
+      {
+        document.location.replace("index.php?Com=Sett");
+        return false;
+      }
+
+      // Ctrl+K  Показать кукисы
+      else if (e.ctrlKey && e.keyCode == 'K'.charCodeAt(0)) 
+      {
+        document.location.replace("index.php?Com=Cook");
+        return false;
+      }
+      
+      // Ctrl+P  Распечатать страницу (функцию предоставляет браузер)
+ 
+      // Ctrl+Q  Показать параметры запроса к сайту
+      else if (e.ctrlKey && e.keyCode == 'Q'.charCodeAt(0)) 
+      {
+        document.location.replace("index.php?Com=Parm");
+        return false;
+      }
+
+      // Ctrl+R  Перегрузить сайт без параметров
+      else if (e.ctrlKey && e.keyCode == 'R'.charCodeAt(0)) 
+      {
+        document.location.replace("index.php?Com=Remv");
+        return false;
+      }
+
+      // Ctrl+S  Сохранить страницу сайта (функцию предоставляет браузер Chrome‎)
+
+      // Ctrl+Z  Передать значение переменной в PHP
+      else if (e.ctrlKey && e.keyCode == 'Z'.charCodeAt(0)) 
+      {
+        var idi = 1352;
+        document.location.replace("index.php?idi="+idi);
+        return false;
+      }
+      
+    }
+  }
+
